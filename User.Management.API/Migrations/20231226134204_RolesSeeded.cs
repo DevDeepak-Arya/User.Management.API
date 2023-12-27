9e621a0ac47e88a91d0370c13425cca3ce467448
DevDeepak-Arya <aryadeepak087.da@gmail.com>
2023-12-27 17:41:53 +0530

    Add project files.

---
+++ User.Management.API/Migrations/20231226134204_RolesSeeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User.Management.API.Migrations
+{
+    public partial class RolesSeeded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2d6a6491-f954-4be2-aa8b-6c496c753454", "3", "HR", "HR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "94c16e29-cb4e-41f1-b839-162a3aea49cc", "1", "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a01a0d7a-b43e-40d4-9987-9412cd59698f", "2", "User", "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2d6a6491-f954-4be2-aa8b-6c496c753454");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "94c16e29-cb4e-41f1-b839-162a3aea49cc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a01a0d7a-b43e-40d4-9987-9412cd59698f");
+        }
+    }
+}
---
+++ User.Management.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace User.Management.API.Models
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            this.SeedRoles(modelBuilder);
+        }
+
+        private void SeedRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>().HasData(
+            new IdentityRole() { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+            new IdentityRole() { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" },
+             new IdentityRole() { Name = "HR", ConcurrencyStamp = "3", NormalizedName = "HR" }
+            );
+        }
+    }
+}
